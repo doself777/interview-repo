46d0e389111d15eaa47f3a2921665023c30cd0cd
Srinivas Vallabhaneni <srinuvallabhaneni7@gmail.com>
2021-06-10 14:53:26 -0700

    Update TribonacciSolution.java
    Solution

--- problems/src/main/java/tribonacci/interview/TribonacciSolution.java
+++ problems/src/main/java/tribonacci/interview/TribonacciSolution.java
@@ -3,8 +3,23 @@ package tribonacci.interview;
 public class TribonacciSolution {
 
     public int tribonacci(int n) {
+        
+        if(n==0){
+            return 0;
+        }
+       
+        int var1=0;
+        int var2=1;
+        int var3=1;
+        
+        for(int i=3;i<=n;i++){
+            int sum = var1+var2+var3;
+            var1 = var2;
+            var2 = var3;
+            var3 = sum;
+        }
 
-        return 0;
+        return var3;
 
     }
 }
